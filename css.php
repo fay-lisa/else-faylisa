ec40af7fe483b74efc0ea67d396af5dd3f3de05b
fay-lisa <fay.lisa@gmail.com>
2017-09-18 16:26:00 -0700

    creating css page, headers
    creating the css page, adding code pens to the cards, writing.
    adding includes for the head, header, footer.
    adding classes per page to make small alterations.
    adding default hover and focus state to text links
    commenting out test code to see how it breaks
    making the cards global, instead of on the home page only
    changing nav for mobile to appear at the bottom, for easier tapping on
    phones. reverts to the top on iPad and larger.
    changing files from html to php

---
+++ css.php
@@ -0,0 +1,73 @@
+<?php include("includes/head-all-else.html");?>
+
+  <body class="css">
+  <?php include("includes/header.html");?>
+    <main role="main">
+      <section>
+        <h1 aria-label="Page Title">CSS</h1>
+
+        <p>I write CSS + HTML. I use LESS, SASS, and vanilla CSS, depending on project. I've been doing this since 1997. Yes, I've written code for IE5. And 5.5. And through the IE7 release that broke every single exception we'd figured out to work around 5's issues. Nothing like trial by fire to spark the 'now, how can I?' engine. </p>
+        <p>This taught me I will never stop learning and re-learning code. Between an employer and my personal projects I code upwards of 12+ hours a day - mostly for the act of learning itself - because I'm driven to.</p>
+        <p>I bridge the gap between pure design, and programming. I take a static design, breathe life and movement and semantics into it so programmers can breathe data into it and everyone can access it.</p>
+        <p>I am a coder. This is, professionally, nearly all I've done since 1997. I've taken OCD from a place of 'have to', to a place of 'do I get to?'. Every day is an adventure in code, every single discovery is as exciting as the first time I made something appear on a web page with just CSS. I absolutely <span class="yellow-emphasis">LOVE what I do</span>.</p>
+        <p>Very soon, I will take the writing I've done my entire life, and put it in a room with my experience with CSS + HTML. I have much to share.</p>
+      </section>
+
+      <section>
+        <article>
+          <div class="cardholder">
+            <div class="card">
+              <h3 class="primary-button yellow" >Animated Form Fields</h3>
+              <p data-height="200" data-theme-id="light" data-slug-hash="QKqrvP" data-default-tab="css,result" data-user="fay-lisa" data-embed-version="2" data-pen-title="CSS3 animated form fields" data-preview="true" class="codepen"></p>
+                <p><a href="https://codepen.io/fay-lisa/pen/QKqrvP/">CSS3 Animated Form Fields: See the Pen</a></p>
+                <script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
+            </div>
+          </div>
+          <div class="cardholder second">
+            <div class="card">
+              <h3 class="primary-button blue">FadeIn + Stay Animation</h3>
+              <p data-height="200" data-theme-id="light" data-slug-hash="QKWkLA" data-default-tab="css,result" data-user="fay-lisa" data-embed-version="2" data-pen-title="Fade-In and Stay animation CSS3" data-preview="true" class="codepen"></p>
+                <p><a href="https://codepen.io/fay-lisa/pen/QKWkLA/">CSS3 Fade-In and Stay animation: See the CodePen</a></p>
+            </div>
+          </div>
+          <div class="cardholder">
+            <div class="card">
+              <h3 class="primary-button green">Holy Grail w/Flex</h3>
+              <p data-height="200" data-theme-id="light" data-slug-hash="ZpGvzL" data-default-tab="css,result" data-user="fay-lisa" data-embed-version="2" data-pen-title="Flex-box 'holy grail' layout" data-preview="true" class="codepen"></p>
+                <p><a href="https://codepen.io/fay-lisa/pen/ZpGvzL/">Flex-box 'holy grail' layout: See the CodePen</a></p>
+            </div>
+          </div>
+        </article>
+
+        <article>
+          <div class="cardholder">
+            <div class="card">
+              <h3 class="primary-button pink">Carousel</h3>
+              <p data-height="200" data-theme-id="light" data-slug-hash="vXRoGo" data-default-tab="css,result" data-user="fay-lisa" data-embed-version="2" data-pen-title="Carousel with css transition, links within panels, diff bkg options" data-preview="true" class="codepen"></p>
+              <p><a href="https://codepen.io/fay-lisa/pen/vXRoGo/">Carousel with css transition, links within panels, diff bkg options: See the CodePen</a></p>
+            </div>
+          </div>
+          <div class="cardholder second">
+
+              <div class="card">
+                <h3 class="primary-button neutral">Holy Grail Old School</h3>
+                <p data-height="200" data-theme-id="light" data-slug-hash="pEWWyE" data-default-tab="css,result" data-user="fay-lisa" data-embed-version="2" data-pen-title="CSS only 'holy grail' layout, no flex" data-preview="true" class="codepen"></p>
+                <p><a href="https://codepen.io/fay-lisa/pen/pEWWyE/">CSS only 'holy grail' layout, old school: See the CodePen for desktop layout</a></p>
+              </div>
+
+            </div>
+
+            <div class="cardholder second">
+              <div class="card">
+                <h3 class="primary-button yellow">Git Project</h3>
+
+                  <p>The CSS behind this site, if you're curious. Written in LESS, though I considered using SASS. My current office uses SASS, so I'm a bit ambidexterous there.</p>
+                  <a href="https://github.com/fay-lisa/else-faylisa" target="_blank">Github Project: Else faylisa site</a>
+              </div>
+            </div>
+          </article>
+        </section>
+      </main>
+    <?php include("includes/footer.html");?>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php include("includes/head-home.html");?>
+<body class="home">
+  <?php include("includes/header.html");?>
+
+  <main role="main">
+    <section class="home-intro">
+      <div class="block-animation">
+        <h1>My name is <span class="emphasis">Fay-Lisa</span>.</h1>
+        <p>I design. I write CSS + HTML.</p>
+        <p>I <span class="emphasis">LOVE</span> what I do. So I work hard to do it well.</p>
+      </div>
+
+      <div class="home-cta">
+        <div class="cta-box">
+          <a href="/">design</a>
+        </div>
+
+        <div class="cta-box">
+          <a href="/">code</a>
+        </div>
+
+        <div class="imum">
+          <img src="images/placeholder.png">
+        </div>
+      </div>
+    </section>
+
+    <section class="home-details">
+      <h2>Alright enough nonsense what is it you do, woman?</h2>
+
+      <article>
+        <div class="cardholder">
+          <a href="/">
+            <div class="card">
+              <h3 class="primary-button yellow" >I write css</h3>
+              <p>I speak it fluently. I love it, and how it evolves, like I love my mother tongue.</p>
+              <!-- <p>I've built custom frameworks for award-winning apps. I've built a reusable app framework to whitelabel and rebrand. I've built frameworks for WordPress themes, and I've built them for fun. </p> -->
+              <p>I can herd errant CSS, and I can fix just about anything people do to CSS when angry.</p>
+              <!-- <p>I've been at this since 1997. CSS is poetry, it's oxygen, it's love and it's the vessel through which I can make the world a better place. There is nothing I would rather be doing.</p> -->
+            </div>
+          </a>
+          <!-- <a href="/" class="primary-button" role="button">But wait, there's more</a> -->
+        </div>
+        <div class="cardholder second">
+          <a href="/">
+            <div class="card">
+              <h3 class="primary-button blue">I design</h3>
+              <!-- <p>I studied psychology to understand how the mind works. I use this to understand how the mind sees the
+                tools I build.</p>
+                <p>I'm fascinated by how colour changes a mood and can instill trust faster than we're conscious of.
+                  I love the feel of shapes, of depth and soul that goes into thoughtful design.</p> -->
+                <p>Design is love of art, and form, of function and of reaching out and touching someone you've never met.</p>
+                <p>I've been at design since 1997</p>
+            </div>
+          </a>
+        </div>
+        <div class="cardholder">
+          <a href="/">
+            <div class="card">
+              <h3 class="primary-button green">Mostly, I bridge gaps</h3>
+              <p>In all but one company I've worked for in the last 12 years, I have been the bridge that spans design, and programming.</p>
+              <p>Bringing design's vision and programming's function to life.</p>
+            </div>
+          </a>
+        </div>
+      </article>
+
+      <h3>Find out what's behind door number 1, 2 and 3</h3>
+      </section>
+    </main>
+
+  <?php include("includes/footer.html");?>
